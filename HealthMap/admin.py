0ec9a71a7913f7f7392647f6cbc94ea840c5125d
Tony Chan <tchan2work@gmail.com>
2026-01-31 17:15:34 -0600

    added models and registered through admin

--- HealthMap/admin.py
+++ HealthMap/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MedService, User, History # import models from models.py
 
-# Register your models here.
+# register all models into the admin server
+admin.site.register(MedService)
+admin.site.register(User)
+admin.site.register(History)
--- HealthMap/models.py
+++ HealthMap/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+class MedService(models.Model):
+    """
+        Has information regarding health clinic
+        Name, Location, Email, Number, Boolean if they take appointments or not
+    """
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    number = models.CharField(max_length=200) # has to be char because we have dashes (-) but not sure maybe? idk
+    appointments_required = models.BooleanField(default=True) # defaulted to true if we do not know
+
+    class Meta: # default ordering by name
+        ordering = ["name"]
+        constraints = [models.UniqueConstraint(fields=["name", "location"], name="unique_name_location")]
+
+    def __str__(self):
+        return f"{self.name} ({self.location})"
+
+
+class User(models.Model):
+    """
+    Has information regarding the actual user
+    Username, email, password, access tokens
+    For user accounts
+    """
+
+    username = models.CharField(max_length=200, unique=True) # true so people can't have same username
+    email = models.EmailField(max_length=200, unique=True) # true so people can't have same emails as well (unique users)
+    password = models.CharField(max_length=200)
+    access_token = models.CharField(max_length=200, unique=True) # each access token is different and constantly gets updated
+
+    class Meta:
+        ordering = ["username"] # default order everything by username
+
+    def __str__(self):
+        return f"{self.username}"
+
+class History(models.Model):
+    """
+    Has recorded chat history that user creates so they can look back on
+    """
+    user = models.ForeignKey(User, related_name="history", on_delete=models.CASCADE) # every user has diff chat history therefore we use foreign keys
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True) # date and time that chat was made
+
+    class Meta:
+        ordering = ["-date"] # default odering by date
+
+    def __str__(self):
+        return f"{self.user.username} @ {self.date}"
