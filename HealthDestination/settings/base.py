88de14d5cd4ef76797d758b67b7416ff76653432
Tony Chan <tchan2work@gmail.com>
2026-02-04 21:41:21 -0600

    Set up the split settings, secrets, project documentation. Section 1

--- HealthDestination/settings/base.py
+++ HealthDestination/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from HealthDestination.secrets_environment import env # import env file from secrets_environment.py
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y85r&!ye6p@3t5h4-vrio&ppahg*q+bo(bm#)=#b6p0_z*g_ip'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -74,12 +72,6 @@ WSGI_APPLICATION = 'HealthDestination.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
