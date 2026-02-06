a9d0849b3b507e0734f0b64b335403698d3b0a35
Jason Wu <jwu249@illinois.edu>
2026-02-06 17:37:53 -0600

    part 2 section b + added appointments to html

--- HealthMap/urls.py
+++ HealthMap/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import hospital_list, hospital_list_render
+from .views import hospital_list, hospital_list_render, MedServiceBaseView, MedServiceListView
 
 urlpatterns = [
+    # FBV routes
     path("hospitals/manual/", hospital_list, name="hospital-list-manual"),
     path("hospitals/render/", hospital_list_render, name="hospital-list-render"),
+    # CBV routes
+    path("services/cbv-base/", MedServiceBaseView.as_view(), name="service-cbv-base"),
+    path("services/cbv-generic/", MedServiceListView.as_view(), name="service-cbv-generic"),
 ]
--- HealthMap/views.py
+++ HealthMap/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import ListView
+from .models import MedService
 
 # Create your views here.
 
@@ -11,3 +14,17 @@ def hospital_list(request):
 # 2 HTTPResponse (Render)
 def hospital_list_render(request):
     return render(request, "render/hospital_list.html")
+
+
+# 3 Base CBV (inherit from View)
+class MedServiceBaseView(View):
+    def get(self, request):
+        services = MedService.objects.all()
+        return render(request, "render/Service List.html", {"services": services})
+
+
+# 4 Generic CBV (ListView)
+class MedServiceListView(ListView):
+    model = MedService
+    template_name = "render/Service List.html"
+    context_object_name = "services"
