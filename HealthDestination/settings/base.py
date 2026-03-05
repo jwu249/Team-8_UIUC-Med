c97e4a8b7482bf0394b06878da20b5a3fc63e59e
Jason Wu <jwu249@illinois.edu>
2026-03-05 14:05:51 -0600

    ui fix

--- HealthDestination/settings/base.py
+++ HealthDestination/settings/base.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles' #needed for static files
 # a3 section 3: project-level static dir (css/images)
